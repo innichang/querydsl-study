d50832dfc1b67fefebb14be466424d21a3b43350
innichang <inni.chang95@gmail.com>
2023-08-07 11:48:52 +0900

    optional 테스트 추가

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -49,4 +49,5 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 //    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
 //    int bulkAgePlus(@Param("age") int age);
 
+
 }
